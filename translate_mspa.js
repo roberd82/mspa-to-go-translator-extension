e55c46a907bcb5dda84ba625a0ca9b9a475a2262
roberd82 <roberd82.dev@gmail.com>
2026-01-21 16:40:25 +0100

    titles, gifs, texts, commands

---
+++ translate_mspa.js
@@ -0,0 +1,49 @@
+// link to lang_info.json, needs to make changeable from extension settings
+var lang_info_url = "https://gitea.roberd.me/forditasok/mspa-magyarul/raw/branch/main/lang_info.json";
+// for offline testing, comment out before commit
+/*var lang_info = JSON.parse(`{
+	"lang": "Magyar",
+	"code": "hu",
+	"assets_dir_url": "https://gitea.roberd.me/forditasok/mspa-magyarul/raw/branch/main/HS_HU/Assets/",
+	"data_dir_url": "https://gitea.roberd.me/forditasok/mspa-magyarul/raw/branch/main/HS_HU/huData/",
+	"data_files": {
+		"translation_MSPA": "MSPA_HU.json"
+	}
+}`);*/
+
+(async () => {
+	// get lang_info, comment this out when offline testing
+	const lang_info = await getJson(lang_info_url)
+	// urls to translation data
+	const translation_MSPA_url = lang_info['data_dir_url'] + lang_info['data_files']['translation_MSPA'];
+	// root url to assets tree
+	const translated_assets_url = lang_info['assets_dir_url'];
+
+	const pageNum = document.getElementById('content').dataset.p;
+
+	const mspa_data = await getJson(translation_MSPA_url);
+
+	document.getElementById('title').innerHTML = mspa_data[pageNum]['title'];
+	//only standard gifs for now
+	const imgs = document.getElementById("media").getElementsByTagName("img");
+	var srcs = [];
+	for (let i = 0; i < imgs.length; i++) {
+		srcs.push(imgs[i].getAttribute("src").split("/mspa/"));
+		imgs[i].src = translated_assets_url + srcs[i][srcs[i].length-1];
+		imgs[i].onerror = function() {
+			imgs[i].src = "/mspa/" + srcs[i][srcs[i].length-1];
+		}
+	}
+
+	document.getElementsByClassName("comic-text").item(0).innerHTML = mspa_data[pageNum]['content']/*.replaceAll("|PESTERLOG|", "")*/;
+	//only works if using mspa page numbers
+	const commands = document.getElementsByClassName("command");
+	for (let i = 0; i < commands.length; i++) {
+		var href = commands[i].firstElementChild.getAttribute("href").split("/");
+		commands[i].firstElementChild.innerHTML = mspa_data[href[href.length-1]]['title'];
+	}
+})()
+
+async function getJson(url) {
+		return await (await fetch(url)).json();
+}
