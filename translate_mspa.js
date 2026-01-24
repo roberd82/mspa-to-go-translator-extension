c38d06f74e16513cede118146ab303bd0238635f
roberd82 <roberd82.dev@gmail.com>
2026-01-24 10:02:55 +0100

    override pages, footer, and menu-bar

--- translate_mspa.js
+++ translate_mspa.js
@@ -1,48 +1,75 @@
+forceMspaNumbering();
+
+redirectToFirstPages();
+
 browser.storage.sync.get("translation_link").then(doThings, onError);
 // everything is in this function, since if we don't have a translation link we can't do anything anyway
 async function doThings(item) {
+	// load the lang_info file
 	const lang_info = await getJson(item['translation_link'] || "https://gitea.roberd.me/forditasok/mspa-magyarul/raw/branch/main/lang_info.json");
-	// maybe should disable the option for homestuck.com page numbering and get the current page from url instead
-	const pageNum = document.getElementById('content').dataset.p;
-	// todo: do something on specific page here
-
-	// load in translation data files
-	const mspa_data = await getJson(lang_info['data_dir_url'] + lang_info['data_files']['translation_MSPA']);
 	
-	document.title = mspa_data[pageNum]['title'] + " - MSPA To Go";
-	// all these loops are needed because of Act 6 Act 5 Act 1 x2
-	const titles = document.querySelectorAll("[id='title']");
-	for (let i = 0; i < titles.length; i++) {
-		titles[i].innerHTML = mspa_data[getNextPageNum(pageNum, i)]['title'];		
+	// find if defined in "replace_pages"
+	let replacePage = -1;
+	for (let i = 0; i < lang_info['replace_pages'].length; i++) {
+		if (window.location.pathname == lang_info['replace_pages'][i]) {
+			replacePage = i;
+			break;
+		}
 	}
-	// gifs and flashes, openbound remains
-	const medias = document.querySelectorAll("[id='media']");
-	var allSrcs = [];
-	for (let i = 0; i < medias.length; i++) {
-		const imgs = medias[i].getElementsByTagName("img");
-		var srcs = [];
-		if (imgs.length > 0) {
-			for (let j = 0; j < imgs.length; j++) {
-				srcs.push(imgs[j].getAttribute("src").substring(6));
-				imgs[j].src = lang_info['assets_dir_url'] + srcs[j];
-				imgs[j].onerror = function() {
-					imgs[j].src = "/mspa/" + allSrcs[i][j];
+	// override page if defined in "replace_pages"
+	if (replacePage > -1) {
+		const newHtml = await fetch(lang_info['replace_pages_url'] + lang_info['replace_pages'][replacePage] + "/index.html");
+		document.body.innerHTML = await newHtml.text();
+		if (lang_info['replace_pages'][replacePage] == "/options") {
+			forceMspaNumbering();
+		}
+		reloadAllScripts();
+	} else {// replace comic content
+		// get page number from current url
+		var pageNum = window.location.pathname.split("/");
+		pageNum = pageNum[pageNum.length-1];
+
+		// load in translation data files
+		const mspa_data = await getJson(lang_info['data_dir_url'] + lang_info['data_files']['translation_MSPA']);
+		
+		// replace browser page title
+		document.title = mspa_data[pageNum]['title'] + " - MSPA To Go";
+
+		// replace titles (all these loops are needed because of Act 6 Act 5 Act 1 x2)
+		const titles = document.querySelectorAll("[id='title']");
+		for (let i = 0; i < titles.length; i++) {
+			titles[i].innerHTML = mspa_data[getNextPageNum(pageNum, i)]['title'];		
+		}
+
+		// replace gifs and flashes (and openbound someday)
+		const medias = document.querySelectorAll("[id='media']");
+		var allSrcs = [];
+		for (let i = 0; i < medias.length; i++) {
+			const imgs = medias[i].getElementsByTagName("img");
+			var srcs = [];
+			if (imgs.length > 0) {
+				for (let j = 0; j < imgs.length; j++) {
+					srcs.push(imgs[j].getAttribute("src").substring(6));
+					imgs[j].src = lang_info['assets_dir_url'] + srcs[j];
+					imgs[j].onerror = function() {
+						imgs[j].src = "/mspa/" + allSrcs[i][j];
+					}
 				}
 			}
+			allSrcs.push(srcs);
 		}
-		allSrcs.push(srcs);
 
-		const flashes = medias[i].getElementsByTagName("ruffle-embed");
+		const flashes = document.getElementsByTagName("ruffle-embed");
 		if (flashes.length > 0) {
-			for (let j = 0; j < flashes.length; j++) {
-				const newSrc = lang_info['assets_dir_url'] + flashes[j].getAttribute("src").substring(6);
+			for (let i = 0; i < flashes.length; i++) {
+				const newSrc = lang_info['assets_dir_url'] + flashes[i].getAttribute("src").substring(6);
 				// check if exists
 				fetch(newSrc, { method: 'HEAD' })
 				.then(response => {
 					if (response.ok) {
-						const newFlash = flashes[j].cloneNode(true);
+						const newFlash = flashes[i].cloneNode(true);
 						newFlash.setAttribute("src", newSrc);
-						flashes[j].replaceWith(newFlash);
+						flashes[i].replaceWith(newFlash);
 					}
 				})
 				.catch(() => {
@@ -50,112 +77,126 @@ async function doThings(item) {
 				});
 			}
 		}
-	}
 
-	const texts = document.getElementsByClassName("comic-text");
-	var allTextSrcs = [];
-	for (let i = 0; i < texts.length; i++) {
-		texts[i].innerHTML = mspa_data[getNextPageNum(pageNum, i)]['content']/*.replaceAll("|PESTERLOG|", "")*/;
+		// fix imported images and links in the text
+		const texts = document.getElementsByClassName("comic-text");
+		var allTextSrcs = [];
+		for (let i = 0; i < texts.length; i++) {
+			texts[i].innerHTML = mspa_data[getNextPageNum(pageNum, i)]['content']/*.replaceAll("|PESTERLOG|", "")*/;
 
-		const links = texts[i].getElementsByTagName("a");
-		for (let j = 0; j < links.length; j++) {
-			if (links[j].getAttribute("href").includes("http://www.mspaintadventures.com/")) {
-				links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("http://www.mspaintadventures.com/", "/mspa/"));
-			} else if (links[j].getAttribute("href").includes("http://mspaintadventures.com/")) {
-				links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("http://mspaintadventures.com/", "/mspa/"));
-			}
-			// sbahj links
-			if (links[j].getAttribute("href").includes("/mspa/sweetbroandhellajeff/")) {
-				links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("/mspa/sweetbroandhellajeff/", "/sbahj"));
-				if (links[j].getAttribute("href").includes("comoc.php")) {
-					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("comoc.php", ""));
+			const links = texts[i].getElementsByTagName("a");
+			for (let j = 0; j < links.length; j++) {
+				if (links[j].getAttribute("href").includes("http://www.mspaintadventures.com/")) {
+					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("http://www.mspaintadventures.com/", "/mspa/"));
+				} else if (links[j].getAttribute("href").includes("http://mspaintadventures.com/")) {
+					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("http://mspaintadventures.com/", "/mspa/"));
 				}
-				if (links[j].getAttribute("href").includes("?cid=")) {
-					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("?cid=", "/"));
-				}
-			}
-			// comic links
-			if (links[j].getAttribute("href").includes("/mspa/?s=")) {
-				links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("/mspa/?s=", "/read/"));
-				if (links[j].getAttribute("href").includes("&p=")) {
-					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("&p=", "/"));
-				}
-			}
-
-			// external links
-			if (links[i].getAttribute("href").includes("/archive/external/")) {
-				const split = links[i].getAttribute("href").split("/archive/external/");
-				links[i].href = lang_info['external_links'][split[split.length-1]];
-			}
-		}
-		// todo: redirect scraps, storyfiles the same way as media to check if exists
-
-		const imgs =  texts[i].getElementsByTagName("img");
-		var srcs = [];
-		for (let j = 0; j < imgs.length; j++) {
-			// first redirect mspaintadventures.com to /mspa
-			if (imgs[j].getAttribute("src").includes("http://www.mspaintadventures.com/")) {
-				imgs[j].setAttribute("src", imgs[j].getAttribute("src").replaceAll("http://www.mspaintadventures.com/", "/mspa/"));
-			} else if (imgs[j].getAttribute("src").includes("http://mspaintadventures.com/")) {
-				imgs[j].setAttribute("src", imgs[j].getAttribute("src").replaceAll("http://mspaintadventures.com/", "/mspa/"));
-			}
-
-			if (imgs[j].getAttribute("src").includes("/mspa/")) {
-				srcs.push(imgs[j].getAttribute("src").substring(6));
-				imgs[j].src = lang_info['assets_dir_url'] + srcs[j];
-				imgs[j].onerror = function() {
-					imgs[j].src = "/mspa/" + allTextSrcs[i][j];
-				}
-			}
-		}
-		allTextSrcs.push(srcs);
-	}
-
-	// only works if using mspa page numbers
-	const commandses = document.getElementsByClassName("commands");
-	for (let i = 0; i < commandses.length; i++) {
-		const commands = commandses[i].getElementsByClassName("command");
-		for (let j = 0; j < commands.length; j++) {
-			const split = commands[j].firstElementChild.getAttribute("href").split("/");
-			if (split.length > 1) {
-				commands[j].firstElementChild.innerHTML = mspa_data[split[split.length-1]]['title'];
-			}
-		}
-	}
-
-	// footnotes
-	for (let k = 0; k < document.querySelectorAll("[id='page']").length; k++) {		// this loop is needed for Act 6 Act 5 Act 1 x2
-		for (let i = 0; i < lang_info['footnote_files'].length; i++) {
-			const footnote = await getJson(lang_info['data_dir_url'] + lang_info['footnote_files'][i]);
-			if (typeof footnote[getNextPageNum(pageNum, k)] != 'undefined') {
-				for (let j = 0; j < footnote[getNextPageNum(pageNum, k)].length; j++) {
-					const note = document.createElement("div");
-					note.innerHTML = footnote[getNextPageNum(pageNum, k)][j]['content'];
-					note.id = "content";
-					note.className = "comic-text";
-
-					if (footnote[getNextPageNum(pageNum, k)][j]['author'] != 'undefined') {
-						const author = document.createElement("span");
-						author.innerHTML = footnote[getNextPageNum(pageNum, k)][j]['author'];
-						author.setAttribute("style", "font-weight: 300;font-size: 10px;font-family: Verdana,Arial,Helvetica,sans-serif;display: flex;justify-content: flex-end;position: relative;top: 12px;margin-top: -12px;color: #979797;");
-						// style from uhc
-						note.appendChild(author);
+				// sbahj links
+				if (links[j].getAttribute("href").includes("/mspa/sweetbroandhellajeff/")) {
+					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("/mspa/sweetbroandhellajeff/", "/sbahj"));
+					if (links[j].getAttribute("href").includes("comoc.php")) {
+						links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("comoc.php", ""));
 					}
+					if (links[j].getAttribute("href").includes("?cid=")) {
+						links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("?cid=", "/"));
+					}
+				}
+				// comic links
+				if (links[j].getAttribute("href").includes("/mspa/?s=")) {
+					links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("/mspa/?s=", "/read/"));
+					if (links[j].getAttribute("href").includes("&p=")) {
+						links[j].setAttribute("href", links[j].getAttribute("href").replaceAll("&p=", "/"));
+					}
+				}
 
-					const fotnoteContent = document.createElement("div");
-					fotnoteContent.id = "page";
+				// external links
+				if (links[i].getAttribute("href").includes("/archive/external/")) {
+					const split = links[i].getAttribute("href").split("/archive/external/");
+					links[i].href = lang_info['external_links'][split[split.length-1]];
+				}
+			}
+			// todo: redirect scraps, storyfiles the same way as media to check if exists
 
-					const footnoteOuter = document.createElement("div");
-					footnoteOuter.id = "page-outer";
+			const imgs =  texts[i].getElementsByTagName("img");
+			var srcs = [];
+			for (let j = 0; j < imgs.length; j++) {
+				// first redirect mspaintadventures.com to /mspa
+				if (imgs[j].getAttribute("src").includes("http://www.mspaintadventures.com/")) {
+					imgs[j].setAttribute("src", imgs[j].getAttribute("src").replaceAll("http://www.mspaintadventures.com/", "/mspa/"));
+				} else if (imgs[j].getAttribute("src").includes("http://mspaintadventures.com/")) {
+					imgs[j].setAttribute("src", imgs[j].getAttribute("src").replaceAll("http://mspaintadventures.com/", "/mspa/"));
+				}
 
-					fotnoteContent.appendChild(note);
-					footnoteOuter.appendChild(fotnoteContent);
+				if (imgs[j].getAttribute("src").includes("/mspa/")) {
+					srcs.push(imgs[j].getAttribute("src").substring(6));
+					imgs[j].src = lang_info['assets_dir_url'] + srcs[j];
+					imgs[j].onerror = function() {
+						imgs[j].src = "/mspa/" + allTextSrcs[i][j];
+					}
+				}
+			}
+			allTextSrcs.push(srcs);
+		}
 
-					document.getElementById("footer").parentNode.insertBefore(footnoteOuter, document.getElementById("footer"));
+		// replace commands, only works with mspa page numbers
+		const commandses = document.getElementsByClassName("commands");
+		for (let i = 0; i < commandses.length; i++) {
+			const commands = commandses[i].getElementsByClassName("command");
+			for (let j = 0; j < commands.length; j++) {
+				const split = commands[j].firstElementChild.getAttribute("href").split("/");
+				if (split.length > 1) {
+					commands[j].firstElementChild.innerHTML = mspa_data[split[split.length-1]]['title'];
 				}
 			}
 		}
+
+		// add footnotes
+		for (let k = 0; k < document.querySelectorAll("[id='page']").length; k++) {		// this loop is needed for Act 6 Act 5 Act 1 x2
+			for (let i = 0; i < lang_info['footnote_files'].length; i++) {
+				const footnote = await getJson(lang_info['data_dir_url'] + lang_info['footnote_files'][i]);
+				if (typeof footnote[getNextPageNum(pageNum, k)] != 'undefined') {
+					for (let j = 0; j < footnote[getNextPageNum(pageNum, k)].length; j++) {
+						const note = document.createElement("div");
+						note.innerHTML = footnote[getNextPageNum(pageNum, k)][j]['content'];
+						note.id = "content";
+						note.className = "comic-text";
+
+						if (footnote[getNextPageNum(pageNum, k)][j]['author'] != 'undefined') {
+							const author = document.createElement("span");
+							author.innerHTML = footnote[getNextPageNum(pageNum, k)][j]['author'];
+							author.setAttribute("style", "font-weight: 300;font-size: 10px;font-family: Verdana,Arial,Helvetica,sans-serif;display: flex;justify-content: flex-end;position: relative;top: 12px;margin-top: -12px;color: #979797;");
+							// style from uhc
+							note.appendChild(author);
+						}
+
+						const fotnoteContent = document.createElement("div");
+						fotnoteContent.id = "page";
+
+						const footnoteOuter = document.createElement("div");
+						footnoteOuter.id = "page-outer";
+
+						fotnoteContent.appendChild(note);
+						footnoteOuter.appendChild(fotnoteContent);
+
+						document.getElementById("footer").parentNode.insertBefore(footnoteOuter, document.getElementById("footer"));
+					}
+				}
+			}
+		}
+
 	}
+
+	// replace footer and menu-bar
+	if (document.getElementById("menu-bar").firstElementChild.innerHTML == "WORTHLESS GARBAGE.") {		// caliborn check
+		replaceElementFromHtml("menu-bar", lang_info['replace_pages_url'] + "/" + lang_info['replace_menu-bar-caliborn']);
+	} else if (document.getElementById("menu-bar").getElementsByTagName('a').length > 20) {			// Act 6 Act 5 Act 1 x2 check
+		let newHtml = await fetch(lang_info['replace_pages_url'] + "/" + lang_info['replace_menu-bar']);
+		let newInnerHtml = await newHtml.text();
+		document.getElementById("menu-bar").innerHTML = newInnerHtml + "<div class=\"candycorn\"></div>" + newInnerHtml;
+	} else {
+		replaceElementFromHtml("menu-bar", lang_info['replace_pages_url'] + "/" + lang_info['replace_menu-bar']);
+	}
+	replaceElementFromHtml("footer", lang_info['replace_pages_url'] + "/" + lang_info['replace_footer']);
 }
 
 function onError(error) {
@@ -173,4 +214,69 @@ function getNextPageNum(pageNum, i) {
 		pageNumNum = "0".repeat(pageNum.length-pageNumNum.length) + pageNumNum;
 	}
 	return pageNumNum;
+}
+
+function forceMspaNumbering() {
+	// spawncamp message box
+	if (document.getElementById("message-viz-links") != null) {
+		if (!document.getElementById("message-viz-links").checked) {
+			document.getElementById("message-viz-links").click();
+		}
+		document.getElementById("message-viz-links").disabled = true;
+		document.getElementById("message-box-dismiss").click();
+	}
+	if (window.location.pathname == "/options") {
+		if (document.getElementById("viz-links").checked) {
+			document.getElementById("viz-links").click();
+			document.getElementsByTagName("form").item(0).submit();
+		}
+		document.getElementById("viz-links").disabled = true;
+	}
+}
+
+function reloadAllScripts() {	// in case an overridden page has new scripts that need to execute
+	const scripts = document.querySelectorAll('script');
+	
+	scripts.forEach((oldScript) => {
+		const newScript = document.createElement('script');
+		
+		Array.from(oldScript.attributes).forEach(attr => {
+			newScript.setAttribute(attr.name, attr.value);
+		});
+		
+		newScript.appendChild(
+			document.createTextNode(oldScript.innerHTML)
+		);
+
+		if (oldScript.parentNode) {
+			oldScript.parentNode.replaceChild(newScript, oldScript);
+		}
+	});
+}
+
+async function replaceElementFromHtml(elementID, linkToHtml) {		// the file the link points to should contain the new innerHTML only
+	const newHtml = await fetch(linkToHtml);
+	document.getElementById(elementID).innerHTML = await newHtml.text();
+}
+
+function redirectToFirstPages(params) {
+	switch (window.location.pathname) {
+		case "/read/6":
+			window.location.replace("/read/6/001901");
+			break;
+		case "/read/5":
+			window.location.replace("/read/5/001893");
+			break;
+		case "/read/4":
+			window.location.replace("/read/4/000219");
+			break;
+		case "/read/2":
+			window.location.replace("/read/2/000136");
+			break;
+		case "/read/1":
+			window.location.replace("/read/2/000002");
+			break;
+		default:
+			break;
+	}
 }
