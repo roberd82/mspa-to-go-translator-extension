3e9f0087d0f7c04345d38204c672fe7d6a245a01
roberd82 <roberd82.dev@gmail.com>
2026-02-02 20:21:48 +0100

    only remove if actually a new line

--- translate_mspa.js
+++ translate_mspa.js
@@ -207,7 +207,9 @@ async function doTheTranslateyThing(item) {
 				let allTextHrefs = [];
 				for (let i = 0; i < texts.length; i++) {
 					texts[i].innerHTML = mspa_data[getPageNumOffset(pageNum, i)]['content'].replaceAll("|PESTERLOG|", "").replaceAll("|SPRITELOG|", "").replaceAll("|RECAP LOG|", "").replaceAll("|JOURNALOG|", "").replaceAll("|DIALOGLOG|", "").replaceAll("|SRIOUSBIZ|", "").replaceAll("|TRKSTRLOG|", "");
-					texts[i].firstElementChild.remove();
+					if (texts[i].firstElementChild.tagName === 'BR') {
+						texts[i].firstElementChild.remove();
+					}
 
 					const links = texts[i].getElementsByTagName("a");
 					let hrefs = [];
